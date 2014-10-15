0415e35910038a5db8c04f65a52c93def4081325
Rob Kimball <rkimball@elevatedthird.com>
2014-10-15 16:04:04 -0600

    Attach js externally rather than rendering inline

---
+++ js/sharpspring-page.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Adds SharpSpring page tracking
+ *
+ * @author: rgkimball
+ */
+
+Drupal.behaviors.sharpSpring = {
+  attach: function(settings, context) {
+    var domain = Drupal.settings.sharpspring.domain,
+      id = Drupal.settings.sharpspring.id;
+
+    var _ss = _ss || [];
+    _ss.push(['_setDomain', 'https://' + domain + '/net']);
+    _ss.push(['_setAccount', id]);
+    _ss.push(['_trackPageView']);
+    (function() {
+      var ss = document.createElement('script');
+      ss.type = 'text/javascript'; ss.async = true;
+      ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + '/client/ss.js?ver=1.1.1';
+      var scr = document.getElementsByTagName('script')[0];
+      scr.parentNode.insertBefore(ss, scr);
+    })();
+  }
+};
