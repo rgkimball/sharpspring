614043beccc3524fe2ed87de035ca3687fd73e3d
Joe Flores <mojiferous@mojiferous.com>
2015-01-29 13:19:53 -0700

    Adding custom segmentation field and javascript handling

--- js/drupal_sharpspring.js
+++ js/drupal_sharpspring.js
@@ -12,21 +12,28 @@
  *   response object returned from Sharpspring
  */
 function ssDrupalCallback(resp) {
-  if (resp && resp.contact) {
+  var sendData = {};
 
-    (function ($, Drupal, window, document) {
-      // This allows additional modules to define their own JS handler functions.
-      if (Drupal.settings.sharpspring.calledFuncs) {
-        for(var n = 0; n < Drupal.settings.sharpspring.calledFuncs.length; n++) {
-          var fn = window[Drupal.settings.sharpspring.calledFuncs[n]];
+  if (resp) {
+    if (resp.contact) {
+      sendData = resp.contact;
+    } else if (resp.lead) {
+      sendData = resp.lead;
+    }
+  }
 
-          if (typeof fn === 'function') {
-            // SharpSpring module handler functions should accept
-            // a response object as a variable.
-            fn(resp.contact);
-          }
+  (function ($, Drupal, window, document) {
+    // This allows additional modules to define their own JS handler functions.
+    if (Drupal.settings.sharpspring.calledFuncs) {
+      for(var n = 0; n < Drupal.settings.sharpspring.calledFuncs.length; n++) {
+        var fn = window[Drupal.settings.sharpspring.calledFuncs[n]];
+
+        if (typeof fn === 'function') {
+          // SharpSpring module handler functions should accept
+          // a response object as a variable.
+          fn(sendData);
         }
       }
-    })(jQuery, Drupal, this, this.document);
-  }
+    }
+  })(jQuery, Drupal, this, this.document);
 }
