57839a580b9228dfc769af10714ae7781a085cc5
Joe Meersman <choicelildice@gmail.com>
2015-02-18 16:09:08 -0700

    Javascript cleanup to put the sharpspring callback functions in Drupal.behaviors, and to store the sharpspring response in Drupal.settings for easy retrieval.

--- js/drupal_sharpspring.js
+++ js/drupal_sharpspring.js
@@ -18,20 +18,27 @@ function ssDrupalCallback(resp) {
     } else if (resp.lead) {
       sendData = resp.lead;
     }
+    console.log(resp);
+    Drupal.settings.sharpspring.response = sendData;
+    Drupal.attachBehaviors();
   }
+}
 
-  (function ($, Drupal, window, document) {
-    // This allows additional modules to define their own JS handler functions.
-    if (Drupal.settings.sharpspring.calledFuncs) {
-      for(var n = 0; n < Drupal.settings.sharpspring.calledFuncs.length; n++) {
-        var fn = window[Drupal.settings.sharpspring.calledFuncs[n]];
+(function ($) {
+  Drupal.behaviors.sharpspringfns = {
+    attach: function (context, settings) {
+      // This allows additional modules to define their own JS handler functions.
+      if (Drupal.settings.sharpspring.calledFuncs && Drupal.settings.sharpspring.response) {
+        for (var n = 0; n < Drupal.settings.sharpspring.calledFuncs.length; n++) {
+          var fn = window[Drupal.settings.sharpspring.calledFuncs[n]];
 
-        if (typeof fn === 'function') {
-          // SharpSpring module handler functions should accept
-          // a response object as a variable.
-          fn(sendData);
+          if (typeof fn === 'function') {
+            // SharpSpring module handler functions should accept
+            // a response object as a variable.
+            fn(Drupal.settings.sharpspring.response);
+          }
         }
       }
     }
-  })(jQuery, Drupal, this, this.document);
-}
+  }
+})(jQuery);
