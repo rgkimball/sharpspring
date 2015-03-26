50b5dbd1c7762d0891c702dab9e091ec31230815
Rob Kimball <rkimball@elevatedthird.com>
2015-03-26 14:13:38 -0600

    Remove inline js, add domain & id to settings object, & use #attached over drupal_add_js for settings & script
    Per community feedback: https://www.drupal.org/node/2368989#comment-9719141

--- js/drupal_sharpspring.js
+++ js/drupal_sharpspring.js
@@ -26,21 +26,46 @@ function ssDrupalCallback(resp) {
   }
 }
 
-(function ($) {
+(function ($, window) {
+
+  $(function() {
+    var sharpspring = Drupal.settings.sharpspring || false;
+
+    if (sharpspring) {
+      var _ss = window._ss || [];
+      _ss.push(['_setDomain', 'https://' + sharpspring.domain + '/net']);
+      _ss.push(['_setAccount',sharpspring.id]);
+      _ss.push(['_setResponseCallback', ssDrupalCallback]);
+      _ss.push(['_trackPageView']);
+
+      var ss = document.createElement('script');
+      ss.type = 'text/javascript'; ss.async = true;
+      ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + sharpspring.domain + '/client/ss.js';
+      var scr = document.getElementsByTagName('script')[0];
+      scr.parentNode.insertBefore(ss, scr);
+
+      window._ss = _ss;
+    }
+
+  });
+
   Drupal.behaviors.sharpspringfns = {
     attach: function (context, settings) {
+      var sharpspring = settings.sharpspring;
+
       // This allows additional modules to define their own JS handler functions.
-      if (Drupal.settings.sharpspring.calledFuncs && Drupal.settings.sharpspring.response) {
-        for (var n = 0; n < Drupal.settings.sharpspring.calledFuncs.length; n++) {
-          var fn = window[Drupal.settings.sharpspring.calledFuncs[n]];
+      if (sharpspring.calledFuncs && sharpspring.response) {
+        for (var n = 0; n < sharpspring.calledFuncs.length; n++) {
+          var fn = window[sharpspring.calledFuncs[n]];
 
           if (typeof fn === 'function') {
             // SharpSpring module handler functions should accept
             // a response object as a variable.
-            fn(Drupal.settings.sharpspring.response);
+            fn(sharpspring.response);
           }
         }
       }
+
     }
   }
-})(jQuery);
+})(jQuery,window);
